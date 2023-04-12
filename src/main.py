25217df7374253b4378a2fa22caa77fb290337b6
Nephty <211260@umons.ac.be>
2023-04-12 16:24:10 +0200

    added a few things in the grammar, working on a useable grammar & interpretation
    added boolean, operator, comparisons (> < = !=), integer
    
    i split the grammars in two files : one with the entire grammar up to now (dumbo.lark) and one with a 'progressing' grammar where i try to add rules and get them working (dumbo_progress.lark)
    
    in dumbo_progress.lark, i have a few rules working : 'programme', 'dumbo bloc', 'expression list', 'string_expression', 'string_list' and 'string_list_interior'
    
    i also have a few rules not working but on track : the 'print' in 'expression' can be parsed and i think it returns the appropriate value (but im not sure) and the 'for variable in string_list do expression list endfor' in 'expression' can be parsed but I have no idea what to do in the for_string method (do we iterate and apply the operations (if so what string do we return ?), do we even have to do anything ? im completely lost)

--- src/main.py
+++ src/main.py
@@ -1,58 +1,88 @@
-from lark import Lark, Transformer
-import sys
+import os
 
-dumbo_grammar = r"""
-    ?programme: txt 
-                | txt programme
-                | dumbo_bloc 
-                | dumbo_bloc programme
-                
-    TXT : /(?!.*\{\{).*/  // contient pas {{
-    
-    ?dumbo_bloc: "{{" expression_list "}}"
-    
-    ?expression_list: expression ";" expression_list
-                      | expression ";"
-    
-    ?expression: "print" string_expression -> print
-                | "for" variable "in" string_list 
-                  "do" expression_list "endfor" -> for_string
-                | "for" variable "in" variable 
-                  "do" expression_list "endfor" -> for_variable
-                | variable ":=" string_expression -> assign
-                | variable ":=" string_list -> assign_list
-    
-    ?string_expression: string
-                        | variable
-                        | string_expression "." string_expression -> concat
-                    
-    ?string_list: "(" string_list_interior ")"
-    
-    ?string_list_interior: string 
-                            | string "," string_list_interior
-    
-    ?VARIABLE : /[a-zA-Z_]\w*/  // lettre ou underscore + (lettre ou nombre ou underscore)*
-    ?STRING : "'" /[^']*?/ "'"  // ' + ((tout sauf single quote)*)? + '
-"""
+from lark import Lark, Transformer, Token, Tree
+import sys
+from pathlib import Path
+
 # TODO : vérifier que la grammaire fonctionne comme ça, faudra peut-être ajouter des noms aux règles
 # TODO : compléter la grammaire
 
 
-def parse_data(data):
+class DumboTransformer(Transformer):
+    def print_variable(self, args):
+        return self.print(args)
+
+    def print_string(self, args):
+        return self.print(args)
+
+    def print(self, args):
+        return args[0].value
+
+    def for_string(self, args):
+        strings = ""
+        for arg in args[1].children:
+            while type(arg) == Tree:
+                arg = arg.children
+            if type(arg) == Token:
+                strings += arg.value[1:-1]
+                strings += ", "
+            elif type(arg) == list:
+                for token in arg:
+                    strings += token.value[1:-1]
+                    strings += ", "
+            else:
+                strings += str(arg)[1:-1]
+                strings += ", "
+        strings = strings[:-2]
+        return f"for {args[0].value} in {strings} do"
+
+    def for_variable(self, args):
+        pass
+
+    def assign(self, args):
+        pass
+
+    def assign_list(self, args):
+        pass
+
+    def concat(self, args):
+        pass
+
+
+def parse_data(data, data_filename):
     """
     Parse the data file and initialise the variables
     :param data: The data file
+    :param data_filename The name of the data file
     """
-    with open(filename_data, 'r') as f:
+    with open(data_filename, 'r') as f:
         data = f.read()
 
     # TODO : Parse the data file and initialise the variables
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
+
+
+    if len(sys.argv) == 1:
+        texts = ["i 4m s0m€ b4s!¢ ŧ€xŧ w!ŧħ CAPS 4nd $ymb0l$",
+                 "{{ print abc; }}",
+                 "{{ for var in ('a', 'b', 'c') do print var; endfor; }}"]
+        parser = Lark(open(Path(__file__).parent.absolute() / "resources" / "dumbo_progress.lark"), start="programme", parser="lalr", transformer=DumboTransformer())
+        for text in texts:
+            parsed = parser.parse(text)
+            print(f"   o  parsing '{text}'")
+            print(repr(parsed))
+            if type(parsed) == Tree:
+                print(parsed.pretty()[:-1])
+            print()
+        exit(0)
+
+
+
+    elif len(sys.argv) != 3:
         print('Usage: python3 main.py <data> <template>')
         exit(1)
-    filename_data = sys.argv[1]
-    filename_template = sys.argv[2]
-    # TODO : Regarder si il vaut mieux envoyer ligne par ligne ou tout le fichier d'un coup
+
+    data_content = open(Path(os.getcwd()) / sys.argv[1], "r").readlines()
+    template_content = open(Path(os.getcwd()) / sys.argv[2], "r").readlines()
