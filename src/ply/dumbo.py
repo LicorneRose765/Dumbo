bf0fb705c22efd6f2f90e2ee4ca1c2642aff9cc5
Nephty <211260@umons.ac.be>
2023-04-24 18:01:43 +0200

    working on lark
    got basic tokens working with ply
    
    how tf do you escape the TXT thing

--- src/ply/dumbo.py
+++ src/ply/dumbo.py
@@ -1,11 +1,29 @@
 import ply.lex as lex
+import ply.yacc as yacc
 import os
 from lark import Lark, Transformer, Token, Tree
 import sys
 from pathlib import Path
 
+# *-------------------------------------------------------------------------------------------------------------------*
+#     VARS
+# *-------------------------------------------------------------------------------------------------------------------*
+
+start = "PROGRAM"
+
+# *-------------------------------------------------------------------------------------------------------------------*
+#     TOKENS
+# *-------------------------------------------------------------------------------------------------------------------*
+
+reserved = {
+    "print": "PRINT",
+    "for": "FOR",
+    "in": "IN",
+    "do": "DO",
+    "endfor": "ENDFOR"
+}
+
 tokens = (
-    "PROGRAM",
     "DUMBO_BLOCK",
     "EXPRESSION_LIST",
     "EXPRESSION",
@@ -15,13 +33,20 @@ tokens = (
     "TXT",
     "STRING",
     "VARIABLE"
-)
+) + tuple(reserved.values())
+
+# *-------------------------------------------------------------------------------------------------------------------*
+#     TOKEN REGEX
+# *-------------------------------------------------------------------------------------------------------------------*
 
 t_TXT = r"(?!.*\{\{).+"
 t_STRING = r"'[^']*'"
 t_VARIABLE = r"[a-zA-Z_]\w*"
 t_ignore = ' \t'
 
+# *-------------------------------------------------------------------------------------------------------------------*
+#     TOKEN METHODS
+# *-------------------------------------------------------------------------------------------------------------------*
 
 def t_newline(t):
     r"\n+"
@@ -31,6 +56,38 @@ def t_newline(t):
 def t_error(t):
     print(f"Illegal character : {t.value[0]}")
     t.lexer.skip(1)
+
+# *-------------------------------------------------------------------------------------------------------------------*
+#     GRAMMAR METHODS
+# *-------------------------------------------------------------------------------------------------------------------*
+
+
+def p_program_single(p):
+    """
+    PROGRAM : TXT
+            | DUMBO_BLOCK
+    """
+    print("Encountered PROGRAM : TXT | DUMBO_BLOCK")
+    p[0] = p[1]  # TODO : pas sûr
+
+
+def p_program_double(p):
+    """
+    PROGRAM : TXT PROGRAM
+            | DUMBO_BLOCK PROGRAM
+            | STRING
+    """
+    print("Encountered PROGRAM : TXT PROGRAM | DUMBO_BLOCK PROGRAM")
+    p[0] = p[1] + p[2]  # TODO : pas sûr
+
+
+def p_error(p):
+    print(f"Syntax error in input {p} !")
+
+
+# *-------------------------------------------------------------------------------------------------------------------*
+#     MAIN
+# *-------------------------------------------------------------------------------------------------------------------*
 
 
 if __name__ == "__main__":
@@ -45,6 +102,17 @@ if __name__ == "__main__":
     import sys
 
     lexer = lex.lex()
-    lexer.input(sys.stdin.read())
+    lexer.input(input())
     for token in lexer:
         print(f"line {token.lineno} : token '{token.value}' (type '{token.type}')")
+
+    parser = yacc.yacc(start=start, debug=True)
+    while True:
+        try:
+            s = input('calc > ')
+        except EOFError:
+            break
+        if s == "": break
+        if not s: continue
+        result = parser.parse(s)
+        print(result)
